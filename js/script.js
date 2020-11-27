2e14a7e5015f73b4526048702a7de6c66b1a7bc8
Caio <caio.student@outlook.com>
2020-11-26 23:48:06 -0300

    adicionando regra de negocio

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
 const input = document.getElementById('input');
+const spanList = document.getElementsByTagName("span");
+spanList[0].innerText = 'Fora';
+console.log();
 
+var textos = ["Fora", "de", "contexto"];
 
+var id = 0;
 
 function word(e) {
+  
   var last =  e[String(e).length -1];
-  if (last !== " "){
-    console.log(e);
-  }
-  else {
+  if (last == " "){
     input.value = "";
+    var sizeWord = 0;
+    id++;
+    return;
   }
+
+  var  sizeWord= e.length; 
+  compareWord(e, sizeWord, id);
+  console.log(e);
+  console.log(sizeWord + '<-')
 }
 
 
-/**
- * 1. Limpar digitador ao dar espaço.
+
+function compareWord(word, size, id){
+  var trueWord = String(textos[id]).slice(0, size);
+  console.log('PALAVRA:' + trueWord);
+  if (trueWord != word) {
+
+  }
+
+}
+/*
  * 2. colocar a proxima palavra em cinza.
  */
