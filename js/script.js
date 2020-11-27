9494d2a522018796c39f32dcaca36ec713209488
Caio <caio.student@outlook.com>
2020-11-27 00:37:13 -0300

    finalização

--- js/script.js
+++ js/script.js
@@ -1,38 +1,57 @@
 const input = document.getElementById('input');
 const spanList = document.getElementsByTagName("span");
 spanList[0].innerText = 'Fora';
-console.log();
 
 var textos = ["Fora", "de", "contexto"];
-
 var id = 0;
+var sizeWord = 0;
+var correct = false;
 
 function word(e) {
-  
   var last =  e[String(e).length -1];
   if (last == " "){
-    input.value = "";
-    var sizeWord = 0;
-    id++;
+    finishWord(correct);
     return;
   }
 
   var  sizeWord= e.length; 
   compareWord(e, sizeWord, id);
-  console.log(e);
-  console.log(sizeWord + '<-')
 }
 
+function finishWord(correct) {
+  input.value = "";
+  sizeWord = 0;
+  removeColor(id, "bgGray");
+  removeColor(id, "bgRed");
 
+  if (correct) {
+    addColor(id, "success");
+  }else {
+    addColor(id, "failure");
+  }
+  id++;
+  addColor(id, "bgGray");
+}
 
 function compareWord(word, size, id){
   var trueWord = String(textos[id]).slice(0, size);
-  console.log('PALAVRA:' + trueWord);
+
   if (trueWord != word) {
-
+    removeColor(id, "bgGray");
+    addColor(id, "bgRed");
+    correct = false;
+  }else{
+    removeColor(id, "bgRed");
+    addColor(id, "bgGray");
+    correct = true;
   }
-
 }
-/*
- * 2. colocar a proxima palavra em cinza.
- */
+
+
+function addColor(n, colorClass) {
+  spanList[n].classList.add(colorClass);
+}
+
+function removeColor(n, colorClass) {
+  spanList[n].classList.remove(colorClass);
+}
